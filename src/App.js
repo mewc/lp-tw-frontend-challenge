c41e3c5de14f7bec3ddc89d8df5f74de482d62c4
mewc <mikecarter771@gmail.com>
2018-12-17 14:14:09 +1100

    update ui to be a bit nicer -
    wont need much more than this...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import TwitterList from "./TwitterList";
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
+
 
 class App extends Component {
   render() {
-    return (
+      const { classes } = this.props;
+
+      return (
       <div className="App">
-        <h1><a href="https://liveperson.com">LIVEPERSON TWITTER CHALLENGE APP</a></h1>
+          <div className={classes.root}>
+              <AppBar position="static">
+                  <Toolbar>
+                      <Typography variant="h6" color="inherit" className={classes.grow}>
+                          LIVEPERSON TWITTER CHALLENGE APP
+                      </Typography>
+                      <Button color="inherit"><a href={'https://mewc.info'} style={{color: '#fff'}}>mewc</a></Button>
+                  </Toolbar>
+              </AppBar>
+          </div>
         <TwitterList/>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/TwitterList.js
+++ src/TwitterList.js
@@ -1,39 +1,66 @@
-import React, { Component } from 'react';
-//https://ant.design/components/list/
-import { List } from 'antd';
+import React, {Component} from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
 
-const data = [
-    {
-        title: 'Ant Design Title 1',
+const styles = theme => ({
+    root: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
+        padding: '10px',
     },
-    {
-        title: 'Ant Design Title 2',
-    },
-    {
-        title: 'Ant Design Title 3',
-    },
-    {
-        title: 'Ant Design Title 4',
-    },
-];
+});
 
-class App extends Component {
+class TwitterList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {tweets: []}
+    }
+
+    componentWillMount() {
+        const tweetEndpoint = (process.env.BACKEND_URL) ? process.env.BACKEND_URL + ':' + process.env.PORT + '/twitter'
+            : 'http://lp-tw-backend-challenge.herokuapp.com/twitter';
+        // :'http://localhost:3333/twitter';
+        axios.get(tweetEndpoint)
+            .then((response) => {
+                console.log(response);
+                this.setState({tweets: response.data})
+            })
+            .catch((log) => {
+                console.log(log)
+            })
+    }
+
+
     render() {
-        return (
-            <List
-                itemLayout="horizontal"
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item>
-                        <List.Item.Meta
-                            title={<a href="https://github.com/mewc">{item.title}</a>}
-                            description={'some desc'}
-                        />
-                    </List.Item>
-                )}
-            />
-        );
+        const {classes} = this.props;
+        const {tweets} = this.state;
+        const list = tweets.map((item) => {
+                return <Grid item xs={4} lg={4}>
+                    <Paper className={classes.root} elevation={1} xs={6} alignItems={'center'} style={{margin: '20px'}}>
+                    <Typography variant="h5" component="h3">
+                        {item.user.user_name}
+                    </Typography>
+                    <Typography component="p">
+                        {item.tweet}
+                    </Typography>
+                    <Typography component="caption" style={{fontSize: '10px', color: '#ccc'}}>
+                        {item.created_at}
+                    </Typography>
+                </Paper>
+                </Grid>
+            });
+        return <React.Fragment>
+            <Grid container spacing={24}>
+                {list}
+            </Grid>
+        </React.Fragment>
+
     }
 }
 
-export default App;
+export default withStyles(styles)(TwitterList);
